a1d9f7f5f74649043485e4b3aefd5414e24a2a06
alexi <alexi@DESKTOP-UBRDAEI>
2021-12-09 16:00:17 -0300

    Agregar archivos de proyecto.

---
+++ BlockSteamPrivate/BlockSteamPrivatePlugin.cs
@@ -0,0 +1,37 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockSteamPrivate
+{
+    public class BlockSteamPrivatePlugin : RocketPlugin
+    {
+        public static BlockSteamPrivatePlugin Instance; 
+        protected override void Load()
+        {
+            Instance = this;
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+            Logger.Log("Se han bloqueado los perfiles privados - By Margarita#8172");
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+        }
+
+        private void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+            if(player.SteamProfile.PrivacyState == "private" || player.SteamProfile.PrivacyState == "Private")
+            {
+                Provider.kick(player.CSteamID, "No Esta Permitido El Perfil En Privado");
+            }
+        }
+    }
+}
